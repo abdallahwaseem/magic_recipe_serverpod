6fd18e38a37c3e8ca8993d92b160996118af61f3
Abdallah Marzouk <abdallahwm1@gmail.com>
2025-06-01 07:19:29 +0300

    View and fetch previous endpoints in app

--- magic_recipe_flutter/lib/main.dart
+++ magic_recipe_flutter/lib/main.dart
@@ -1,3 +1,6 @@
+// magic_recipe_flutter/lib/main.dart
+//
+//
 import 'package:magic_recipe_client/magic_recipe_client.dart';
 import 'package:flutter/material.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
@@ -57,12 +60,15 @@ class MyHomePageState extends State<MyHomePage> {
   /// Holds the last result or null if no result exists yet.
   Recipe? _recipe;
 
+  List<Recipe> _recipeHistory = [];
+
   /// Holds the last error message that we've received from the server or null if no
   /// error exists yet.
   String? _errorMessage;
-  bool _loading = false;
 
   final _textEditingController = TextEditingController();
+
+  bool _loading = false;
 
   void _callGenerateRecipe() async {
     try {
@@ -71,13 +77,13 @@ class MyHomePageState extends State<MyHomePage> {
         _recipe = null;
         _loading = true;
       });
-
       final result =
           await client.recipes.generateRecipe(_textEditingController.text);
       setState(() {
         _errorMessage = null;
         _recipe = result;
         _loading = false;
+        _recipeHistory.insert(0, result);
       });
     } catch (e) {
       setState(() {
@@ -89,45 +95,88 @@ class MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    // Get the favourite recipes from the database
+    client.recipes.getRecipes().then((favouriteRecipes) {
+      setState(() {
+        _recipeHistory = favouriteRecipes;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: TextField(
-                controller: _textEditingController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your recipe',
-                ),
+      body: Row(
+        children: [
+          Expanded(
+            child: DecoratedBox(
+              decoration: BoxDecoration(color: Colors.grey[300]),
+              child: ListView.builder(
+                itemCount: _recipeHistory.length,
+                itemBuilder: (context, index) {
+                  final recipe = _recipeHistory[index];
+                  return ListTile(
+                    title: Text(
+                        recipe.text.substring(0, recipe.text.indexOf('\n'))),
+                    subtitle: Text('${recipe.author} - ${recipe.date}'),
+                    onTap: () {
+                      // Show the recipe in the text field
+                      _textEditingController.text = recipe.ingredients;
+                      setState(() {
+                        _recipe = recipe;
+                      });
+                    },
+                  );
+                },
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 16.0),
-              child: ElevatedButton(
-                onPressed: _loading ? null : _callGenerateRecipe,
-                child: _loading
-                    ? const Text('Loading...')
-                    : const Text('Send to Server'),
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your ingredients',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: ElevatedButton(
+                      onPressed: _loading ? null : _callGenerateRecipe,
+                      child: _loading
+                          ? const Text('Loading...')
+                          : const Text('Send to Server'),
+                    ),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child:
+                          // Change the ResultDisplay to use the Recipe object
+                          ResultDisplay(
+                        resultMessage: _recipe != null
+                            ? '${_recipe?.author} on ${_recipe?.date}:\n${_recipe?.text}'
+                            : null,
+                        errorMessage: _errorMessage,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: ResultDisplay(
-                  resultMessage: _recipe != null
-                      ? '${_recipe?.author} on ${_recipe?.date}:\n${_recipe?.text}'
-                      : null,
-                  errorMessage: _errorMessage,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
