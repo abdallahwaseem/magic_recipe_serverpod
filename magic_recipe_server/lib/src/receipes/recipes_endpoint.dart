40c3ebdf26fade45b5c801b536e6d65dd391f1bb
Abdallah Marzouk <abdallahwm1@gmail.com>
2025-06-01 07:53:53 +0300

    Refactor recipe generation logic to use a separate function for content generation

--- magic_recipe_server/lib/src/receipes/recipes_endpoint.dart
+++ magic_recipe_server/lib/src/receipes/recipes_endpoint.dart
@@ -1,6 +1,15 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:magic_recipe_server/src/generated/protocol.dart';
+import 'package:meta/meta.dart';
 import 'package:serverpod/serverpod.dart';
+
+@visibleForTesting
+var generateContent =
+    (String apiKey, String prompt) async => (await GenerativeModel(
+          model: 'gemini-1.5-flash-latest',
+          apiKey: apiKey,
+        ).generateContent([Content.text(prompt)]))
+            .text;
 
 class RecipesEndpoint extends Endpoint {
   Future<Recipe> generateRecipe(Session session, String ingredients) async {
@@ -10,10 +19,6 @@ class RecipesEndpoint extends Endpoint {
     if (geminiApiKey == null) {
       throw Exception('Gemini API key not found');
     }
-    final gemini = GenerativeModel(
-      model: 'gemini-1.5-flash-latest',
-      apiKey: geminiApiKey,
-    );
 
     // A prompt to generate a recipe, the user will provide a free text input with the ingredients
     final prompt =
@@ -21,9 +26,7 @@ class RecipesEndpoint extends Endpoint {
         'of the recipe in the first line, and then the instructions. The recipe should be easy '
         'to follow and include all necessary steps. Please provide a detailed recipe.';
 
-    final response = await gemini.generateContent([Content.text(prompt)]);
-
-    final responseText = response.text;
+    final responseText = await generateContent(geminiApiKey, prompt);
 
     // Check if the response is empty or null
     if (responseText == null || responseText.isEmpty) {
