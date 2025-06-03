7214c7f368cfea70494d03002377f190772dff5a
Abdallah Marzouk <abdallahwm1@gmail.com>
2025-06-03 17:26:48 +0300

    Add 3 points to trigger, schedule, or cancel clean up function

--- magic_recipe_server/lib/src/admin_endpoint.dart
+++ magic_recipe_server/lib/src/admin_endpoint.dart
@@ -1,3 +1,5 @@
+import 'package:magic_recipe_server/server.dart';
+import 'package:magic_recipe_server/src/recipes/remove_deleted_recipe_future_call.dart';
 import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_auth_server/serverpod_auth_server.dart';
 
@@ -20,5 +22,22 @@ class AdminEndpoint extends Endpoint {
 
   Future<void> unblockUser(Session session, int userId) async {
     await Users.unblockUser(session, userId);
+  }
+
+  Future<void> triggerDeletedRecipeCleanup(Session session) async {
+    await RemoveDeletedRecipesFutureCall().invoke(session, null);
+  }
+
+  Future<void> scheduleDeletedRecipeCleanup(Session session) async {
+    await pod.futureCallWithDelay(
+      FutureCallNames.rescheduleRemoveDeletedRecipes.name,
+      null,
+      Duration(seconds: 5),
+    );
+  }
+
+  Future<void> stopCleanupTask(Session session) async {
+    await pod
+        .cancelFutureCall(FutureCallNames.rescheduleRemoveDeletedRecipes.name);
   }
 }
